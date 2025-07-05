ca112ec8feaafb07877ea3fac16e2ee7edf003f1
wingrove210 <vladidermkov330@gmail.com>
2025-07-05 14:21:37 +0300

    modified:   Dockerfile 	modified:   app/core/config.py 	new file:   backup.txt

--- app/core/config.py
+++ app/core/config.py
@@ -41,8 +41,8 @@ class Settings(BaseSettings):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
     model_config = ConfigDict(
-        # env_file=".env",
-        env_file="/Users/vladdremenko/ПРОЕКТЫ/SUNA_BOT/backend/.env",  
+        env_file=".env",
+        # env_file="/Users/vladdremenko/ПРОЕКТЫ/SUNA_BOT/backend/.env",  
         env_file_encoding="utf-8",
         case_sensitive=True,
         cache_strings=False,
